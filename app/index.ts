6da395df1d6555fa8ee04e805671caffee176577
Luciano Montilla <unonueve95@gmail.com>
2024-10-03 15:51:05 -0300

    Initial config

--- app/index.ts
+++ app/index.ts
@@ -1,8 +1,45 @@
 import express, { Express, Request, Response } from "express";
+import cors, { CorsOptions } from "cors";
+import mongoose from "mongoose";
 
 export const app: Express = express();
+const corsOptions: CorsOptions = {
+    origin: "*",
+};
+
+const albumSchema = new mongoose.Schema({
+    title: String,
+    artist: String,
+});
+const Album = mongoose.model("Album", albumSchema);
 
 app.use(express.json());
+app.use(cors(corsOptions));
+
+app.get("/test", async (req: Request, res: Response) => {
+    const albums = await Album.find();
+    if (albums) {
+        res.json(albums);
+    } else {
+        res.status(404).send({ message: "Error finding all albums" });
+    }
+});
+
+app.post("/test", async (req: Request, res: Response) => {
+    const newAlbum = { ...req.body };
+    await Album.create(newAlbum);
+    res.status(201).send({ message: "Successfully created album:", newAlbum });
+});
+
+app.get("/test/:id", async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const album = await Album.findById(id);
+    if (album) {
+        res.status(200).send(album);
+    } else {
+        res.status(404).send({ message: "Error finding album" });
+    }
+});
 
 app.all("*", (req: Request, res: Response) => {
     res.status(404).json({ error: `Route ${req.originalUrl} not found` });
--- server.ts
+++ server.ts
@@ -1,7 +1,18 @@
 import { app } from "./app";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose
+    .connect(MONGO_URL!)
+    .then(() => {
+        console.log("connected to database");
+    })
+    .catch((error) => console.error("connection", error));
 
 app.listen(PORT, () => {
     console.log(`Server running on port: http://${HOST}:${PORT}`);
