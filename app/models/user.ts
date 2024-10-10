9dcbe1a94593c5c3b76e696dc3e9fe9217a25ef0
Luciano Montilla <unonueve95@gmail.com>
2024-10-10 15:08:05 -0300

    Update user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,10 +1,35 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-});
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: ["guest", "user", "admin"],
+            default: "user",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
 
 const User = mongoose.model("user", UserSchema);
 
