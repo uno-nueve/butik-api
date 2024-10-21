bded0e73a6ed173411c1cae71e4f97ef7f2feada
Luciano Montilla <unonueve95@gmail.com>
2024-10-21 18:16:55 -0300

    Create customer model

---
+++ app/models/customer.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const CustomerSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        dni: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        telephone: {
+            type: String,
+            required: true,
+        },
+        purchases: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "Order",
+            },
+        ],
+        paymentMethods: [],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Customer = mongoose.model("customers", CustomerSchema);
