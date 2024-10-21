63a842a4f4f368d0a8816e4ec18c9b02614c2ab0
Luciano Montilla <unonueve95@gmail.com>
2024-10-21 18:08:04 -0300

    Create product model

---
+++ app/models/product.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        stock: {
+            type: Number,
+            required: true,
+        },
+        has_delivery: {
+            type: Boolean,
+            default: false,
+        },
+        pictures: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "Picture",
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Product = mongoose.model("product", ProductSchema);
