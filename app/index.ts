2f6ac3d3542667e7d1e469bc1ee70c52e06a449e
Luciano Montilla <unonueve95@gmail.com>
2024-11-10 20:31:29 -0300

    Create users router

--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import cors, { CorsOptions } from "cors";
 import authRouter from "./routes/auth-router";
 import productsRouter from "./routes/products";
+import usersRouter from "./routes/users";
+import vendorsRouter from "./routes/vendors";
 
 export const app = express();
 const corsOptions: CorsOptions = {
@@ -13,6 +15,8 @@ app.use(cors(corsOptions));
 
 app.use("/api", authRouter);
 app.use("/api", productsRouter);
+app.use("/api", usersRouter);
+app.use("/api", vendorsRouter);
 
 app.all("*", (req: Request, res: Response) => {
     res.status(404).json({ error: `Route ${req.originalUrl} not found` });
---
+++ app/routes/users.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response } from "express";
+import User from "../models/user";
+
+const router = express.Router();
+
+//----------------------------------- CRUD OPERATIONS -----------------------------------//
+
+//INDEX all user records
+router.get("/users", async (req: Request, res: Response) => {
+    try {
+        const users = await User.find();
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send({ message: "Internal server error" });
+    }
+});
+
+//SHOW a single user record by ID
+router.get("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send({ message: "Internal server error" });
+    }
+});
+
+//UPDATE a single user record by ID
+router.put("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id);
+        if (!updatedUser) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        res.status(200).send(updatedUser);
+    } catch (error) {
+        res.status(400).send({ message: "Error updating user", error });
+    }
+});
+
+//REMOVE a single user record by ID
+router.delete("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        res.status(200).send({ message: "User successfully deleted" });
+    } catch (error) {
+        res.status(500).send({ message: "Error deleting user", error });
+    }
+});
+
+export default router;
