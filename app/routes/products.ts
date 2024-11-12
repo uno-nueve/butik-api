23e912d877e071e7d2b7d20615971fab5abec873
Luciano Montilla <unonueve95@gmail.com>
2024-11-12 10:32:18 -0300

    Update products router

--- app/routes/products.ts
+++ app/routes/products.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import Product from "../models/product";
+import Order from "../models/order";
 
 const router = express.Router();
 
@@ -42,7 +43,7 @@ router.post("/products", async (req: Request, res: Response) => {
 });
 
 //UPDATE a single product record form the collection
-router.put("/products/:id", async (req: Request, res: Response) => {
+router.patch("/products/:id", async (req: Request, res: Response) => {
     try {
         const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
@@ -75,9 +76,32 @@ router.delete("/products/:id", async (req: Request, res: Response) => {
 //----------------------------------- BUSINESS LOGIC -----------------------------------//
 
 //UPDATE a product record state when purchasing
-router.put("/products/:id/buy", async (req: Request, res: Response) => {
+router.post("/products/:id/buy", async (req: Request, res: Response) => {
+    const { vendor, customer, quantity } = req.body;
     try {
-        //process order here
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).send({ message: "Product not found" });
+        }
+
+        if (product.stock == 0) {
+            return res.status(400).send({ message: "Product sold out" });
+        }
+
+        product.stock = product.stock - quantity;
+        await product.save();
+
+        const date = new Date();
+        const order = await Order.create({
+            vendor,
+            customer,
+            product: req.params.id,
+            confirmationCode: "214365", //implement middleware to autogenerate random codes
+            issueDate: new Date(),
+            duebyDate: date.setDate(date.getDate() + 7),
+        });
+
+        res.status(201).send({ product, order });
     } catch (error) {
         res.status(400).send({ message: "Error processing order", error });
     }
